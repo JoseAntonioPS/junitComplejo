ff1357c80ca26d1d9c62797a80679b1bd8d4369f
JoseAntonioPS <48015813+JoseAntonioPS@users.noreply.github.com>
2019-06-03 21:19:45 +0200

    Cambio de nombres

--- junitComplejo/test/junitComplejo/AllTests.java
+++ junitComplejo/test/junitComplejo/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ComplejoTest.class, ComplejoTest2.class, ComplejoTestParametrized.class })
+@SuiteClasses({ ComplejoTest.class, Complejo2Test.class, ComplejoParametrizedTest.class })
 public class AllTests {
 
 }
